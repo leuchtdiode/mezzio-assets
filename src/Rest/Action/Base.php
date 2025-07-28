f892a9e73936668019b1f8275e55f7499217767f
Alex Schloegl <alex@try2catch.com>
2025-07-28 17:57:34 +0200

    Changed Response to EmptyResponse

--- src/Rest/Action/Base.php
+++ src/Rest/Action/Base.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Assets\Rest\Action;
 
-use Laminas\Diactoros\Response as DiactorosResponse;
+use Laminas\Diactoros\Response\EmptyResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -21,11 +21,11 @@ abstract class Base implements RequestHandlerInterface
 
 	protected function notFound(): ResponseInterface
 	{
-		return new DiactorosResponse(null, 404);
+		return new EmptyResponse(404);
 	}
 
 	protected function forbidden(): ResponseInterface
 	{
-		return new DiactorosResponse(null, 403);
+		return new EmptyResponse(403);
 	}
 }
